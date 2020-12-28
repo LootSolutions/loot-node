2f875a4c8f30de5f1d056cb5450655edfe0055cb
Tony Sun <tony.sun427@gmail.com>
2020-12-28 11:03:57 -0800

    add nft class creation calling orml nft pallet

--- pallets/nft-token/src/lib.rs
+++ pallets/nft-token/src/lib.rs
@@ -73,21 +73,21 @@ decl_module! {
 
 		/// An example dispatchable that takes a singles value as a parameter, writes the value to
 		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		// #[weight = 10_000 + T::DbWeight::get().writes(1)]
-		// pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
-		// 	// Check that the extrinsic was signed and get the signer.
-		// 	// This function will return an error if the extrinsic is not signed.
-		// 	// https://substrate.dev/docs/en/knowledgebase/runtime/origin
-		// 	let who = ensure_signed(origin)?;
+		#[weight = 10_000 + T::DbWeight::get().writes(1)]
+		pub fn do_something(origin, something: u32) -> dispatch::DispatchResult {
+			// Check that the extrinsic was signed and get the signer.
+			// This function will return an error if the extrinsic is not signed.
+			// https://substrate.dev/docs/en/knowledgebase/runtime/origin
+			let who = ensure_signed(origin)?;
 
-		// 	// Update storage.
-		// 	Something::put(something);
+			// Update storage.
+			Something::put(something);
 
-		// 	// Emit an event.
-		// 	Self::deposit_event(RawEvent::SomethingStored(something, who));
-		// 	// Return a successful DispatchResult
-		// 	Ok(())
-		// }
+			// Emit an event.
+			Self::deposit_event(RawEvent::SomethingStored(something, who));
+			// Return a successful DispatchResult
+			Ok(())
+		}
 
 		// 
 		// "CID": "Vec<u8>"
